c3e2bae7ba99f310fa81ba3ca768c25f1a5bb298
Ali Kamel <alikamel2004@hotmail.com>
2020-08-10 00:57:28 +0100

    lesson 32 on using the controller and the fxml file

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,11 @@
 package sample;
 
+import javafx.scene.control.Button;
+
 public class Controller {
+    public Button button;
+    public void handleButtonClick() {
+        System.out.println("this button is now working");
+        button.setText("hello");
+    }
 }
