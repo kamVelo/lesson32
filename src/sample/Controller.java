b3a504705b053e586f275e2be971557194df2572
Ali Kamel <alikamel2004@hotmail.com>
2020-08-12 17:28:47 +0100

    lesson 35 last lesson on scene builder

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,10 +4,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class Controller {
-    public Button button;
-    public Label label1;
-
-    public void changeText(){
-        label1.setText("this text is changed");
+    public void loginButtonClicked(){
+        System.out.println("User Logged in");
     }
 }
